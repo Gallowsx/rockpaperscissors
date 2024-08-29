f84added9b43cdaca2d9a6516fc1865f838f8cf4
Gallowsx <171734779+Gallowsx@users.noreply.github.com>
2024-08-29 15:30:57 -0500

    Add function to track score, add function to determine winner

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 "use strict";
 
+let computerScore = 0;
+let humanScore = 0;
 const humanChoice = getHumanChoice();
 const computerChoice = getComputerChoice();
-let humanScore;
-let computerScore;
 
 function getComputerChoice() {
     let rockPaperScissors = ["ROCK", "PAPER", "SCISSORS"];
@@ -12,8 +12,8 @@ function getComputerChoice() {
   }
 
 function getHumanChoice() {
-    let userChoice = prompt("(Rock, Paper, or Scissors)Pick One:");
-    return userChoice.toUpperCase();
+    let humanChoice = prompt("(Rock, Paper, or Scissors)Pick One:");
+    return humanChoice.toUpperCase();
 }
 
 function playRound(humanChoice, computerChoice) {
@@ -23,26 +23,52 @@ function playRound(humanChoice, computerChoice) {
             console.log("Draw")
         } else if (computerChoice === "PAPER") {
             console.log("You lose")
+            computerScore++;
         } else if (computerChoice === "SCISSORS") {
             console.log("You win")
+            humanScore++;
         }
         break;
+
     case "PAPER":
         if (computerChoice === "ROCK") {
             console.log("You win")
+            humanScore ++;
         } else if (computerChoice === "PAPER") {
             console.log("Draw")
         } else if (computerChoice === "SCISSORS") {
             console.log("You lose")
+            computerScore ++;
         }
         break;
+
     case "SCISSORS":
         if (computerChoice === "ROCK") {
             console.log("You lose")
+            computerScore++;
         } else if (computerChoice === "PAPER") {
             console.log("You win")
+            humanScore++;
         } else if (computerChoice === "SCISSORS") {
             console.log("Draw")
         }
-        break;
-    
+}}
+
+
+function playGame() {
+
+    playRound(humanChoice, computerChoice);
+    if (computerScore < 5&& humanScore < 5) {
+        alert( 'Next Round!')
+    }
+    else if (humanScore >= 5) {
+        console.log("You win the game!")
+    }
+    else if (computerScore >= 5) {
+        console.log("You lose the game!")
+    }
+    console.log("Human score is", humanScore);
+    console.log("Computer score is", computerScore);
+}
+
+playGame();
